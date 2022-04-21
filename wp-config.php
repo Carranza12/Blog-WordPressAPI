4bb5fc21d8b09ce1e46b11908e9e1023a57ef6ea
Francisco Javier Carranza Esquivel <60407692+Carranza12@users.noreply.github.com>
2022-04-20 23:51:46 -0500

    deploy

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'mrx8tc4mwkgwl7gj' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'ukuslpv98x01advl' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'wls4ts6jcetr13kj' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'bv2rebwf6zzsv341.cbetxkdyhwsb.us-east-1.rds.amazonaws.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
